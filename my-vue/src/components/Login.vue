f7cd9d155fb0a79e87f88951e8ab7bbe8a3421e9
vincent <gw0683@163.com>
2017-12-26 19:41:51 +0800

    my vue project

--- my-vue/src/components/Login.vue
+++ my-vue/src/components/Login.vue
@@ -1,32 +1,69 @@
 <template>
-    <div class="login">
-      <span v-text="msg"></span>
-      <p>
-        <button v-on:click="addClick">Click Me</button>
-      </p>
-    </div>
+  <div class="login">
+    <span v-text="msg" v-show="flag"></span>
+    <p>
+      <button v-on:click="addClick">Click Me</button>
+    </p>
+    <p>
+      <input v-model="newItem" v-on:keyup.enter="addItem"/>
+    </p>
+    <ul>
+      <li v-for="item in items" v-bind:class="{finished: item.isFinished}">
+        <span v-on:click="modifyStatus(item)">{{item.language}}</span>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "login",
-        data(){
-          return{
-             msg : '登录成功'
+  export default {
+    name: "login",
+    data: function () {
+      return {
+        msg: '登录成功',
+        flag: true,
+        items: [
+          {
+            language: 'Java',
+            isFinished: true
+          },
+          {
+            language: 'Python',
+            isFinished: false
+          },
+          {
+            language: 'Vue',
+            isFinished: true
           }
-        },
-        methods:{
-           addClick : function (event) {
-              console.log(this.msg)
-              console.info(event)
-             console.info(event.toElement.textContent)
-           }
-        }
+        ],
+        liClass: 'liClassLog',
+        newItem: ''
+      }
+    },
+    methods: {
+      addClick: function (event) {
+        console.info(event)
+        console.info(event.toElement.textContent)
+      },
+      modifyStatus: function (item) {
+        item.isFinished = !item.isFinished
+      },
+      addItem: function () {
+        this.items.push({
+          language: this.newItem,
+          isFinished: false
+        })
+        this.newItem = ''
+      }
     }
+  }
 </script>
 
 <style scoped>
-  button{
+  .finished{
+    text-decoration: underline;
+  }
+  button {
     color: #42b983;
   }
 </style>
