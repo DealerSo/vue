2dff55ae3a2bdec53809848030f5017590a94bb3
vincent <gw0683@163.com>
2017-12-26 17:46:59 +0800

    my vue project

---
+++ my-vue/src/components/Login.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="login">
+      <span v-text="msg"></span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "login",
+        data(){
+          return{
+             msg : '登录成功'
+          }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ my-vue/src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Hello from '../components/Hello'
+import Login from '../components/Login'
+
+Vue.use(Router)
+
+export default new Router({
+  mode:'history',
+  routes: [
+    {
+      path: '/',
+      name: 'hello',
+      component: Hello
+    },
+    {
+      path:'/login',
+      name:'login',
+      component:Login
+    }
+  ]
+})
