e559b3d1035bb927dfd0737e24ceea46c1d09421
vincent <gw0683@163.com>
2017-12-27 18:51:57 +0800

    wood vue project

---
+++ wood-vue/src/components/Course.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="course">
+      {{msg}}
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "course",
+        data(){
+          return {
+            msg : 'test'
+          }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- wood-vue/src/router/index.js
+++ wood-vue/src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import HelloWorld from '../components/HelloWorld'
+import Course from '../components/Course'
 
 Vue.use(Router)
 
 export default new Router({
+  //请求是无#符号,比如：http://localhost:8080/#/,设置了这个属性http://localhost:8080
+  mode:'history',
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/course',
+      name: 'Course',
+      component: Course
     }
   ]
 })
