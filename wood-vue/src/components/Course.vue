ec49312f05a8bd521b39eb3928fdf271e58939c1
vincent <gw0683@163.com>
2017-12-27 19:00:27 +0800

    wood vue project

--- wood-vue/src/components/Course.vue
+++ wood-vue/src/components/Course.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="course">
-      {{msg}}
+      <div class="enterCourse">
+        <span>请选择学习的课程</span>
+        <input v-model="courseName" v-on:keyup.enter="addCourse"/>
+      </div>
+      <div class="choosedCourse">
+        <div>已选择的课程</div>
+        <div v-for="course in courses">
+          {{course.language}}
+        </div>
+      </div>
   </div>
 </template>
 
@@ -9,7 +18,17 @@
         name: "course",
         data(){
           return {
-            msg : 'test'
+             courses: [],
+             courseName: ''
+          }
+        },
+        methods:{
+          addCourse : function(){
+            this.courses.push({
+               language: this.courseName,
+               isChecked:false
+            }),
+            this.courseName = ''
           }
         }
     }
