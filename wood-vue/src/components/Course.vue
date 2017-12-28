7d6bfd05c0de0e8091d097e7457034b17fd54daa
vincent <gw0683@163.com>
2017-12-28 15:36:59 +0800

    wood vue project

--- wood-vue/src/components/Course.vue
+++ wood-vue/src/components/Course.vue
@@ -16,8 +16,8 @@
             v-on:keyup.enter="addCourse"
         />
 
-        <ul class="task-count">
-          <li>个任务未完成</li>
+        <ul class="task-count" v-show="courses.length">
+          <li>{{courses.length}}个任务未完成</li>
           <li class="action">
             <a class="active" href="#">所有任务</a>
             <a href="#">未完成的任务</a>
@@ -27,13 +27,13 @@
 
         <h3 class="big-title">任务列表：</h3>
         <div class="tasks">
-          <span class="no-task-tip">还没有添加任何任务</span>
+          <span class="no-task-tip" v-show="!courses.length">还没有添加任何任务</span>
           <ul class="todo-list">
             <li class="todo"  v-for="course in courses" >
               <div class="view">
-                <input class="toggle" type="checkbox" />
-                <label>{{ course.language }}</label>
-                <button class="destroy"></button>
+                <input class="toggle" type="checkbox" v-on:click="modifyStatus(course)"/>
+                <label v-bind:class="{completed:course.isChecked}">{{ course.language }}</label>
+                <button class="destroy" v-on:click="removeCourse(course)"></button>
               </div>
             </li>
           </ul>
@@ -47,21 +47,34 @@
 </template>
 
 <script>
+    import Store from '../store/store'
     export default {
         name: "course",
         data(){
           return {
-             courses: [],
+             courses: Store.fetch(),
              courseName: ''
           }
         },
         methods:{
+          // 添加课程
           addCourse : function(){
             this.courses.push({
                language: this.courseName,
                isChecked:false
             }),
+            Store.save(this.courses)
             this.courseName = ''
+          },
+          // 删除课程
+          removeCourse : function (course) {
+              var index = this.courses.indexOf(course);
+              this.courses.splice(index,1)
+              // 删除缓存中的项
+              Store.remove(index)
+          },
+          modifyStatus : function (course) {
+            course.isChecked = !course.isChecked
           }
         }
     }
---
+++ wood-vue/src/store/store.js
@@ -0,0 +1,20 @@
+// 定义常量
+const STORAGE_KEY = "wood_storage";
+
+export default {
+  // 获取缓存
+  fetch : function () {
+    return JSON.parse(window.localStorage.getItem(STORAGE_KEY) || '[]')
+  },
+  save : function (item) {
+    window.localStorage.setItem(STORAGE_KEY,JSON.stringify(item))
+  },
+  remove : function (index) {
+    // 得到数组值
+    var courses = JSON.parse(window.localStorage.getItem(STORAGE_KEY));
+    // 删除该索引所对应的项
+    courses.splice(index,1)
+    //重新setItem3
+    window.localStorage.setItem(STORAGE_KEY,JSON.stringify(courses))
+  }
+}
